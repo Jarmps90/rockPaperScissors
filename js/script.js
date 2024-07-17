7779d5f8acb08e7c16b8f14b88ac86fc0127305a
Jarmps90 <160318973+Jarmps90@users.noreply.github.com>
2024-07-17 22:08:04 +0300

    need to add score function and loop for playRound

--- js/script.js
+++ js/script.js
@@ -1,8 +1,46 @@
-
+let humanScore = 0;
+let computerScore = 0;
+const choices = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
-    const gameChoices = ["rock", "paper", "scissors"];
-    return gameChoices[(Math.floor(Math.random() * gameChoices.length))];
-}
- console.log(getComputerChoice());
+    const computerChoice = choices[(Math.floor(Math.random() * choices.length))];
+    return computerChoice
     
+};
+
+ 
+
+function getHumanChoice() {
+   const humanChoice = window.prompt("Choose rock, paper or scissors ");
+   return humanChoice.toLowerCase;
+};
+
+function whosWinner(humanChoice, computerChoice) {
+if (humanChoice === computerChoice) {
+    return "Tie";
+} else if(
+    (humanChoice === "rock" && computerChoice === "sicssors") ||
+    (humanChoice === "sicssors" && computerChoice === "paper") ||
+    (humanChoice === "paper" && computerChoice === "rock")
+){
+    return "Player"
+ } else {
+    return "Computer"
+}
+};
+
+function playRound(humanChoice, computerChoice) {
+    const result = whosWinner(humanChoice, computerChoice);
+    if (result === "Tie") {
+        return "It's a Tie!"
+    } else if (result === "Player") {
+        return "Congratulations, you won!"
+    } else {
+        return "Sorry, computer won!"
+    }
+}
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+console.log(playRound(humanChoice,computerChoice))
+
